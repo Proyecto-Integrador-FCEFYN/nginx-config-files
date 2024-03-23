ab843579c7880f269e680bb4eea81a59049beb58
dfdavid <fd.dandrea@unc.edu.ar>
2024-03-23 01:52:26 -0300

    ufw deny defaull incoming

--- david_test/config_ufw.sh
+++ david_test/config_ufw.sh
@@ -1,7 +1,10 @@
-# Permitir tráfico en los puertos 80 y 443
+# Permitir tráfico en los puertos 80, 443 y 22
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw allow 22/tcp
+
+# Denegar todo el tráfico entrante
+sudo ufw default deny incoming
 
 # Habilitar UFW para aplicar las reglas
 sudo ufw enable
